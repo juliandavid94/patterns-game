0bd10d444288e4a32c548c9150206d02a7b1e22b
Edgar Junior Castro Escorcia <ecastro@TESSIGESTIONA.LOCAL.COM>
2020-10-16 23:15:11 -0500

    agario whit phaser

---
+++ agario/src/gameObjets/opponent.js
@@ -0,0 +1,14 @@
+class Opponent extends Phaser.GameObjects.Sprite {
+
+    constructor(scene, x, y, type) {
+        super(scene, x, y, type);
+        scene.add.existing(this);
+        scene.physics.world.enable(this);
+        this.body.setCollideWorldBounds(true);
+        this.body.setBounce(1);
+        this.body.setVelocityX(-180);
+        this.body.setVelocityY(Phaser.Math.Between(-120, 120));
+    }
+}
+
+export default Opponent;
---
+++ agario/src/gameObjets/player.js
@@ -0,0 +1,14 @@
+class Player extends Phaser.GameObjects.Sprite {
+
+    constructor(scene, x, y, type) {
+        super(scene, x, y, type);
+        scene.add.existing(this);
+        scene.physics.world.enable(this);
+        this.body.setCollideWorldBounds(true);
+        this.body.setBounce(1);
+
+
+    }
+}
+
+export default Player;
